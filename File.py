9ddf6e4b4668314284c22f32ce125bbb68f43158
VijayWasTaken <88245937+VijayWasTaken@users.noreply.github.com>
2022-11-14 18:48:27 +0000

    Add files via upload

---
+++ File.py
@@ -0,0 +1,6 @@
+import os
+
+source="C:/Users/vijay.LAPTOP-6K2HIQ6S/OneDrive/Desktop/VS Projects Python/Project-115/txt.txt"
+dest="C:/Users/vijay.LAPTOP-6K2HIQ6S/OneDrive/Desktop/VS Projects Python/Project-115/Text.txt"
+
+os.rename(source,dest)
